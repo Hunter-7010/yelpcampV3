f8d579d00ee3796bbafee5c2e972cd1ab5d2c264
Zaki Orya <zakioria@gmail.com>
2023-04-09 15:00:06 +0300

    added mobile support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,17 @@ export default {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        standalone: { raw: "(display-mode: standalone)" },
+      },
+      spacing: {
+        "safe-top": "env(safe-area-inset-top)",
+        "safe-bottom": "env(safe-area-inset-bottom)",
+        "safe-left": "env(safe-area-inset-left)",
+        "safe-right": "env(safe-area-inset-right)",
+      },
+    },
   },
-  plugins: [require("tailwindcss/plugin"),require("@tailwindcss/forms")],
+  plugins: [require("tailwindcss/plugin"), require("@tailwindcss/forms")],
 } satisfies Config;
